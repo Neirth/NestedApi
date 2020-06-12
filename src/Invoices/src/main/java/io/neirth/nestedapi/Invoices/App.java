120495e85afe350780b697229dd67e76bdadd0c9
Sergio Martinez <7413024+Neirth@users.noreply.github.com>
2020-06-12 16:07:41 +0200

    General - Initialize Project

---
+++ src/Invoices/src/main/java/io/neirth/nestedapi/Invoices/App.java
@@ -0,0 +1,13 @@
+package io.neirth.nestedapi.Invoices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/Payments/src/main/java/io/neirth/nestedapi/Payments/App.java
@@ -0,0 +1,13 @@
+package io.neirth.nestedapi.Payments;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
