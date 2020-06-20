921bfe5bec196a7d093827b1964cb363d4041b99
neirth <7413024+Neirth@users.noreply.github.com>
2020-06-20 19:48:46 +0200

    Users - Complete Contries enum with name

--- src/Users/src/main/java/io/neirth/nestedapi/Users/Templates/Country.java
+++ src/Users/src/main/java/io/neirth/nestedapi/Users/Templates/Country.java
@@ -1,42 +1,65 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 NestedApi Project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package io.neirth.nestedapi.Users.Templates;
 
 public enum Country {
     // Country List Standard ISO 3166
-    AF("Afghanistan"), AX("AlandIslands"), AL("Albania"), DZ("Algeria"), AS("AmericanSamoa"), AD("Andorra"),
-    AO("Angola"), AI("Anguilla"), AQ("Antarctica"), AG("AntiguaAndBarbuda"), AR("Argentina"), AM("Armenia"),
+    AF("Afghanistan"), AX("Aland Islands"), AL("Albania"), DZ("Algeria"), AS("American Samoa"), AD("Andorra"),
+    AO("Angola"), AI("Anguilla"), AQ("Antarctica"), AG("Antigua And Barbuda"), AR("Argentina"), AM("Armenia"),
     AW("Aruba"), AU("Australia"), AT("Austria"), AZ("Azerbaijan"), BS("Bahamas"), BH("Bahrain"), BD("Bangladesh"),
     BB("Barbados"), BY("Belarus"), BE("Belgium"), BZ("Belize"), BJ("Benin"), BM("Bermuda"), BT("Bhutan"), BO("Bolivia"),
-    BA("BosniaAndHerzegovina"), BW("Botswana"), BV("BouvetIsland"), BR("Brazil"), IO("BritishIndianOceanTerritory"),
-    BN("BruneiDarussalam"), BG("Bulgaria"), BF("BurkinaFaso"), BI("Burundi"), KH("Cambodia"), CM("Cameroon"), 
-    CA("Canada"), CV("CapeVerde"), KY("CaymanIslands"), CF("CentralAfricanRepublic"), TD("Chad"), CL("Chile"), CN("China"),
-    CX("ChristmasIsland"), CC("CocosKeelingIslands"), CO("Colombia"), KM("Comoros"), CG("Congo"), CD("CongoDemocraticRepublic"),
-    CK("CookIslands"), CR("CostaRica"), CI("CoteDIvoire"), HR("Croatia"), CU("Cuba"), CY("Cyprus"), CZ("CzechRepublic"),
-    DK("Denmark"), DJ("Djibouti"), DM("Dominica"), DO("DominicanRepublic"), EC("Ecuador"), EG("Egypt"), SV("ElSalvador"),
-    GQ("EquatorialGuinea"), ER("Eritrea"), EE("Estonia"), ET("Ethiopia"), FK("FalklandIslands"), FO("FaroeIslands"),
-    FJ("Fiji"), FI("Finland"), FR("France"), GF("FrenchGuiana"), PF("FrenchPolynesia"), TF("FrenchSouthernTerritories"),
+    BA("Bosnia And Herzegovina"), BW("Botswana"), BV("Bouvet Island"), BR("Brazil"), IO("British Indian Ocean Territory"),
+    BN("Brunei Darussalam"), BG("Bulgaria"), BF("Burkina Faso"), BI("Burundi"), KH("Cambodia"), CM("Cameroon"), 
+    CA("Canada"), CV("Cape Verde"), KY("Cayman Islands"), CF("Central African Republic"), TD("Chad"), CL("Chile"), CN("China"),
+    CX("Christmas Island"), CC("Cocos Keeling Islands"), CO("Colombia"), KM("Comoros"), CG("Congo"), CD("Congo Democratic Republic"),
+    CK("Cook Islands"), CR("Costa Rica"), CI("Cote D'Ivoire"), HR("Croatia"), CU("Cuba"), CY("Cyprus"), CZ("Czech Republic"),
+    DK("Denmark"), DJ("Djibouti"), DM("Dominica"), DO("Dominican Republic"), EC("Ecuador"), EG("Egypt"), SV("El Salvador"),
+    GQ("Equatorial Guinea"), ER("Eritrea"), EE("Estonia"), ET("Ethiopia"), FK("Falkland Islands"), FO("Faroe Islands"),
+    FJ("Fiji"), FI("Finland"), FR("France"), GF("French Guiana"), PF("French Polynesia"), TF("French Southern Territories"),
     GA("Gabon"), GM("Gambia"), GE("Georgia"), DE("Germany"), GH("Ghana"), GI("Gibraltar"), GR("Greece"), GL("Greenland"),
-    GD("Grenada"), GP("Guadeloupe"), GU("Guam"), GT("Guatemala"), GG("Guernsey"), GN("Guinea"), GW("GuineaBissau"), GY("Guyana"),
-    HT("Haiti"), HM("HeardIslandMcdonaldIslands"), VA("HolySeeVaticanCityState"), HN("Honduras"), HK("HongKong"), HU("Hungary"),
-    IS("Iceland"), IN("India"), ID("Indonesia"), IR("Iran"), IQ("Iraq"), IE("Ireland"), IM("IsleOfMan"), IL("Israel"), IT("Italy"),
+    GD("Grenada"), GP("Guadeloupe"), GU("Guam"), GT("Guatemala"), GG("Guernsey"), GN("Guinea"), GW("Guinea Bissau"), GY("Guyana"),
+    HT("Haiti"), HM("Heard Island Mcdonald Islands"), VA("Holy See Vatican City State"), HN("Honduras"), HK("Hong Kong"), HU("Hungary"),
+    IS("Iceland"), IN("India"), ID("Indonesia"), IR("Iran"), IQ("Iraq"), IE("Ireland"), IM("Isle Of Man"), IL("Israel"), IT("Italy"),
     JM("Jamaica"), JP("Japan"), JE("Jersey"), JO("Jordan"), KZ("Kazakhstan"), KE("Kenya"), KI("Kiribati"), KR("Korea"), KW("Kuwait"),
-    KG("Kyrgyzstan"), LA("LaoPeoplesDemocraticRepublic"), LV("Latvia"), LB("Lebanon"), LS("Lesotho"), LR("Liberia"), LY("LibyanArabJamahiriya"),
+    KG("Kyrgyzstan"), LA("Lao Peoples Democratic Republic"), LV("Latvia"), LB("Lebanon"), LS("Lesotho"), LR("Liberia"), LY("Libyan Arab Jamahiriya"),
     LI("Liechtenstein"), LT("Lithuania"), LU("Luxembourg"), MO("Macao"), MK("Macedonia"), MG("Madagascar"), MW("Malawi"), MY("Malaysia"),
-    MV("Maldives"), ML("Mali"), MT("Malta"), MH("MarshallIslands"), MQ("Martinique"), MR("Mauritania"), MU("Mauritius"), YT("Mayotte"),
+    MV("Maldives"), ML("Mali"), MT("Malta"), MH("Marshall Islands"), MQ("Martinique"), MR("Mauritania"), MU("Mauritius"), YT("Mayotte"),
     MX("Mexico"), FM("Micronesia"), MD("Moldova"), MC("Monaco"), MN("Mongolia"), ME("Montenegro"), MS("Montserrat"), MA("Morocco"),
-    MZ("Mozambique"), MM("Myanmar"), NA("Namibia"), NR("Nauru"), NP("Nepal"), NL("Netherlands"), AN("NetherlandsAntilles"),
-    NC("NewCaledonia"), NZ("NewZealand"), NI("Nicaragua"), NE("Niger"), NG("Nigeria"), NU("Niue"), NF("NorfolkIsland"),
-    MP("NorthernMarianaIslands"), NO("Norway"), OM("Oman"), PK("Pakistan"), PW("Palau"), PS("PalestinianTerritory"), PA("Panama"), 
-    PG("PapuaNewGuinea"), PY("Paraguay"), PE("Peru"), PH("Philippines"), PN("Pitcairn"), PL("Poland"), PT("Portugal"),
-    PR("PuertoRico"), QA("Qatar"), RE("Reunion"), RO("Romania"), RU("RussianFederation"), RW("Rwanda"), BL("SaintBarthelemy"),
-    SH("SaintHelena"), KN("SaintKittsAndNevis"), LC("SaintLucia"), MF("SaintMartin"), PM("SaintPierreAndMiquelon"), VC("SaintVincentAndGrenadines"),
-    WS("Samoa"), SM("SanMarino"), ST("SaoTomeAndPrincipe"), SA("SaudiArabia"), SN("Senegal"), RS("Serbia"), SC("Seychelles"), SL("SierraLeone"),
-    SG("Singapore"), SK("Slovakia"), SI("Slovenia"), SB("SolomonIslands"), SO("Somalia"), ZA("SouthAfrica"), GS("SouthGeorgiaAndSandwichIsl"),
-    ES("Spain"), LK("SriLanka"), SD("Sudan"), SR("Suriname"), SJ("SvalbardAndJanMayen"), SZ("Swaziland"), SE("Sweden"), CH("Switzerland"),
-    SY("SyrianArabRepublic"), TW("Taiwan"), TJ("Tajikistan"), TZ("Tanzania"), TH("Thailand"), TL("TimorLeste"), TG("Togo"), TK("Tokelau"),
-    TO("Tonga"), TT("TrinidadAndTobago"), TN("Tunisia"), TR("Turkey"), TM("Turkmenistan"), TC("TurksAndCaicosIslands"), TV("Tuvalu"),
-    UG("Uganda"), UA("Ukraine"), AE("UnitedArabEmirates"), GB("UnitedKingdom"), US("UnitedStates"), UM("UnitedStatesOutlyingIslands"),
-    UY("Uruguay"), UZ("Uzbekistan"), VU("Vanuatu"), VE("Venezuela"), VN("VietNam"), VG("VirginIslandsBritish"), VI("VirginIslandsUS"),
-    WF("WallisAndFutuna"), EH("WesternSahara"), YE("Yemen"), ZM("Zambia"), ZW("Zimbabw");
+    MZ("Mozambique"), MM("Myanmar"), NA("Namibia"), NR("Nauru"), NP("Nepal"), NL("Netherlands"), AN("Netherlands Antilles"),
+    NC("New Caledonia"), NZ("New Zealand"), NI("Nicaragua"), NE("Niger"), NG("Nigeria"), NU("Niue"), NF("Norfolk Island"),
+    MP("Northern Mariana Islands"), NO("Norway"), OM("Oman"), PK("Pakistan"), PW("Palau"), PS("Palestinian Territory"), PA("Panama"), 
+    PG("Papua New Guinea"), PY("Paraguay"), PE("Peru"), PH("Philippines"), PN("Pitcairn"), PL("Poland"), PT("Portugal"),
+    PR("Puerto Rico"), QA("Qatar"), RE("Reunion"), RO("Romania"), RU("Russian Federation"), RW("Rwanda"), BL("Saint Barthelemy"),
+    SH("Saint Helena"), KN("Saint Kitts And Nevis"), LC("Saint Lucia"), MF("Saint Martin"), PM("Saint Pierre And Miquelon"), VC("Saint Vincent And Grenadines"),
+    WS("Samoa"), SM("San Marino"), ST("Sao Tome And Principe"), SA("Saudi Arabia"), SN("Senegal"), RS("Serbia"), SC("Seychelles"), SL("Sierra Leone"),
+    SG("Singapore"), SK("Slovakia"), SI("Slovenia"), SB("Solomon Islands"), SO("Somalia"), ZA("South Africa"), GS("South Georgia And Sandwich Isl"),
+    ES("Spain"), LK("SriLanka"), SD("Sudan"), SR("Suriname"), SJ("Svalbard And Jan Mayen"), SZ("Swaziland"), SE("Sweden"), CH("Switzerland"),
+    SY("Syrian Arab Republic"), TW("Taiwan"), TJ("Tajikistan"), TZ("Tanzania"), TH("Thailand"), TL("Timor Leste"), TG("Togo"), TK("Tokelau"),
+    TO("Tonga"), TT("Trinidad And Tobago"), TN("Tunisia"), TR("Turkey"), TM("Turkmenistan"), TC("Turks And Caicos Islands"), TV("Tuvalu"),
+    UG("Uganda"), UA("Ukraine"), AE("United Arab Emirates"), GB("United Kingdom"), US("United States"), UM("United States Outlying Islands"),
+    UY("Uruguay"), UZ("Uzbekistan"), VU("Vanuatu"), VE("Venezuela"), VN("Vietnam"), VG("Virgin Islands British"), VI("Virgin Islands US"),
+    WF("Wallis And Futuna"), EH("Western Sahara"), YE("Yemen"), ZM("Zambia"), ZW("Zimbabw");
 
     private final String name;
 
