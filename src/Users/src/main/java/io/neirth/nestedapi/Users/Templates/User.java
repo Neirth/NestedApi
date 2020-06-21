d30384e70f32b624deec57ec81f20bec09424065
neirth <7413024+Neirth@users.noreply.github.com>
2020-06-21 12:08:29 +0200

    Users - Added Builder Pattern into User Object Template

--- src/Users/src/main/java/io/neirth/nestedapi/Users/Templates/User.java
+++ src/Users/src/main/java/io/neirth/nestedapi/Users/Templates/User.java
@@ -2,6 +2,7 @@ package io.neirth.nestedapi.Users.Templates;
 import java.util.Date;
 
 public class User {
+    private Long id;
     private String name;
     private String surname;
     private String email;
@@ -12,8 +13,93 @@ public class User {
     private String address;
     private String addressInformation;
 
-    public User(String name, String surname, String email, String password, String telephone, Date birthday,
-                Country country, String address, String addressInformation) {
+    public static class Builder {
+        private Long id;
+        private String name;
+        private String surname;
+        private String email;
+        private String password;
+        private String telephone;
+        private Date birthday;
+        private Country country;
+        private String address;
+        private String addressInformation;
+
+        public Builder(Long id) {
+            this.id = id;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+
+            return this;
+        }
+
+        public Builder setSurname(String surname) {
+            this.surname = surname;
+            
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            this.password = password;
+
+            return this;
+        }
+
+        public Builder setTelephone(String telephone) {
+            this.telephone = telephone;
+
+            return this;
+        }
+
+        public Builder setBirthday(Date birthday) {
+            this.birthday = birthday;
+
+            return this;
+        }
+    
+        public Builder setCountry(Country country) {
+            this.country = country;
+
+            return this;
+        }
+    
+        public Builder setAddress(String address) {
+            this.address = address;
+
+            return this;
+        }
+    
+        public Builder setAddressInformation(String addressInformation) {
+            this.addressInformation = addressInformation;
+
+            return this;
+        }
+
+        public User build() {
+            return new User(this.id,
+                            this.name, 
+                            this.surname, 
+                            this.email, 
+                            this.password, 
+                            this.telephone, 
+                            this.birthday, 
+                            this.country, 
+                            this.address, 
+                            this.addressInformation);
+        }
+    }
+
+    public User(Long id, String name, String surname, String email, String password, String telephone, Date birthday,
+        Country country, String address, String addressInformation) {
+        this.id = id;
         this.name = name;
         this.surname = surname;
         this.email = email;
@@ -23,78 +109,45 @@ public class User {
         this.country = country;
         this.address = address;
         this.addressInformation = addressInformation;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getSurname() {
         return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getTelephone() {
         return telephone;
     }
 
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
     public Date getBirthday() {
         return birthday;
-    }
-
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
     }
 
     public Country getCountry() {
         return country;
     }
 
-    public void setCountry(Country country) {
-        this.country = country;
-    }
-
     public String getAddress() {
         return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 
     public String getAddressInformation() {
         return addressInformation;
     }
-
-    public void setAddressInformation(String addressInformation) {
-        this.addressInformation = addressInformation;
-    }
-
 }
