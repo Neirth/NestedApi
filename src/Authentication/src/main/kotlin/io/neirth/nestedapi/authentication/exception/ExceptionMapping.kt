923d0e0034dfd5d655afd328be5e6aa8f213bad5
Sergey Neirth <7413024+Neirth@users.noreply.github.com>
2021-03-02 11:44:12 +0100

    Authentication - Added JSON Standard Error

--- src/Authentication/src/main/kotlin/io/neirth/nestedapi/authentication/exception/ExceptionMapping.kt
+++ src/Authentication/src/main/kotlin/io/neirth/nestedapi/authentication/exception/ExceptionMapping.kt
@@ -23,28 +23,41 @@
  */
 package io.neirth.nestedapi.authentication.exception
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ObjectNode
 import java.net.MalformedURLException
 import javax.ws.rs.core.Response
 import javax.ws.rs.ext.ExceptionMapper
 import javax.ws.rs.ext.Provider
+
 
 @Provider
 class ExceptionMapping : ExceptionMapper<Exception> {
     override fun toResponse(p0: Exception): Response {
         return when (p0) {
             is SecurityException -> {
-                Response.status(Response.Status.FORBIDDEN.statusCode, p0.message).build()
+                Response.status(Response.Status.FORBIDDEN.statusCode).entity(generateJsonResponse(p0)).build()
             }
             is LoginException -> {
-                Response.status(Response.Status.UNAUTHORIZED.statusCode, p0.message).build()
+                Response.status(Response.Status.UNAUTHORIZED.statusCode).entity(generateJsonResponse(p0)).build()
             }
             is MalformedURLException -> {
-                Response.status(Response.Status.BAD_REQUEST.statusCode, p0.message).build()
+                Response.status(Response.Status.BAD_REQUEST.statusCode).entity(generateJsonResponse(p0)).build()
             }
             else -> {
                 println(p0.printStackTrace())
-                Response.status(Response.Status.INTERNAL_SERVER_ERROR.statusCode, p0.message).build()
+                Response.status(Response.Status.INTERNAL_SERVER_ERROR.statusCode).entity(generateJsonResponse(p0)).build()
             }
         }
+    }
+
+    private fun generateJsonResponse(p0: Exception): String {
+        val mapper = ObjectMapper()
+
+        val user: ObjectNode = mapper.createObjectNode()
+        user.put("error", "${p0.cause}")
+        user.put("error_description", "${p0.message}")
+
+        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(user)
     }
 }
